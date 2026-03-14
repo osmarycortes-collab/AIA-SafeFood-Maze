a1450b380d2f18bbdaa8b163763756533f5a58d8
osmarycortes-collab <osmarycortes@gmail.com>
2026-03-14 15:35:32 -0500

    Update juego.js

--- juego.js
+++ juego.js
@@ -35,7 +35,7 @@ const sonidoCorrecto = new Audio(CONFIG.sonidos.correcto)
 const sonidoError = new Audio(CONFIG.sonidos.error)
 const sonidoPuerta = new Audio(CONFIG.sonidos.puerta)
 
-const musicaNivel = new Audio(CONFIG.musicaNiveles[nivelActual-1])
+const musicaNivel = new Audio(CONFIG.musicaNiveles[0])
 musicaNivel.loop = true
 
 const btnMusica = document.getElementById("btnMusica")
