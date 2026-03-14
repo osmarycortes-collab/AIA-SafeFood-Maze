e34158f097d0b598231495c11a93288c5b49f5ec
osmarycortes-collab <osmarycortes@gmail.com>
2026-03-13 19:48:35 -0500

    Update certificate levels and themes formatting

--- datos/certificados.js
+++ datos/certificados.js
@@ -6,52 +6,52 @@
 const CERTIFICADOS = {
 
 1:{
-nivel:"Nivel 1 –  Manejo de Granos",
+nivel:"Nivel 1",
 tema:"BPM – Manejo de Granos"
 },
 
 2:{
-nivel:"Nivel 2 –  Molinería",
+nivel:"Nivel 2 ",
 tema:"BPM – Molinería"
 },
 
 3:{
-nivel:"Nivel 3 – Panadería",
+nivel:"Nivel 3 ",
 tema:"BPM – Panadería"
 },
 
 4:{
-nivel:"Nivel 4 – Confitería",
+nivel:"Nivel 4 ",
 tema:"HACCP Básico"
 },
 
 5:{
-nivel:"Nivel 5 – Vegetales",
+nivel:"Nivel 5 ",
 tema:"Sanitización de Vegetales"
 },
 
 6:{
-nivel:"Nivel 6 – Jugos",
+nivel:"Nivel 6 ",
 tema:"Control de Procesos HACCP"
 },
 
 7:{
-nivel:"Nivel 7 –  Lácteos",
+nivel:"Nivel 7 ",
 tema:"Inocuidad en Lácteos"
 },
 
 8:{
-nivel:"Nivel 8 – Cárnicos",
+nivel:"Nivel 8 ",
 tema:"Inocuidad en Cárnicos"
 },
 
 9:{
-nivel:"Nivel 9 – Avícola",
+nivel:"Nivel 9 ",
 tema:"Inocuidad Avícola"
 },
 
 10:{
-nivel:"Nivel 10 – Productos Listos",
+nivel:"Nivel 10 ",
 tema:"FSSC 22000 Básico en productos listos"
 }
 
