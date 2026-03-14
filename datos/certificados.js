aae687a7fa1a5e40e60351b4cc3e08dbc0b1b17d
osmarycortes-collab <osmarycortes@gmail.com>
2026-03-14 14:32:09 -0500

    Update certificados.js

--- datos/certificados.js
+++ datos/certificados.js
@@ -3,56 +3,61 @@
    AIA SafeFood Maze
 ========================================= */
 
-const CERTIFICADOS = {
+const CERTIFICADOS = [
 
-1:{
+{
 nivel:"Nivel 1",
 tema:"BPM – Manejo de Granos"
 },
 
-2:{
+{
 nivel:"Nivel 2 ",
 tema:"BPM – Molinería"
 },
 
-3:{
+{
 nivel:"Nivel 3 ",
 tema:"BPM – Panadería"
 },
 
-4:{
+{
 nivel:"Nivel 4 ",
 tema:"HACCP Básico"
 },
 
-5:{
+{
 nivel:"Nivel 5 ",
 tema:"Sanitización de Vegetales"
 },
 
-6:{
+{
+nivel:"Nivel 5 ",
+tema:"Sanitización de Vegetales"
+},
+
+{
 nivel:"Nivel 6 ",
 tema:"Control de Procesos HACCP"
 },
 
-7:{
+{
 nivel:"Nivel 7 ",
 tema:"Inocuidad en Lácteos"
 },
 
-8:{
+{
 nivel:"Nivel 8 ",
 tema:"Inocuidad en Cárnicos"
 },
 
-9:{
+{
 nivel:"Nivel 9 ",
 tema:"Inocuidad Avícola"
 },
 
-10:{
+{
 nivel:"Nivel 10 ",
 tema:"FSSC 22000 Básico en productos listos"
 }
 
-}
+]
